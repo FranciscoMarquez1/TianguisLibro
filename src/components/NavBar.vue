9ef90d9ebfda0cb5054cc69d8c31b9c0c0145e5e
mmaxBA <max29ba@gmail.com>
2020-04-07 19:09:02 +0000

    Added Bootstrap to the project

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,16 @@
 <template>
-    <h1>Hola maxi</h1>
+    <div class="row">
+        <h1 class="col">Hola maxi</h1>
+        <h1 class="col">Hola maxi</h1>
+
+        <div>
+            <b-button>Button</b-button>
+            <b-button variant="danger">Button</b-button>
+            <b-button variant="success">Button</b-button>
+            <b-button variant="outline-primary">Button</b-button>
+        </div>
+    </div>
+
 </template>
 
 <script>
